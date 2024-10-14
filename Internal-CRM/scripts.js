8a625cce88601d674a3cd7736fa2b54fdc970732
srikar kudurmalla <srikar2985969c@gmail.com>
2024-10-14 21:38:14 +0530

    add client popup

--- Internal-CRM/scripts.js
+++ Internal-CRM/scripts.js
@@ -82,3 +82,8 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 });
+document.querySelectorAll('.config-main-section-sidebar ul li a').forEach(link => {
+    if (link.href === window.location.href) {
+        link.parentElement.classList.add('active');
+    }
+});
